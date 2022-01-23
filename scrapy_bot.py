1219584e74d3dc3f644ca944166d417c3685bcd2
HaroldDRF <95robots@gmail.com>
2022-01-23 01:41:59 -0700

    get dev connections from google

--- scrapy_bot.py
+++ scrapy_bot.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome("E:\Portfolio_Proj\Python\mapping\chromedriver")
 sleep(0.5)
@@ -23,3 +25,19 @@ sleep(0.5)
 
 driver.find_element_by_xpath('//button[text()="Sign in"]').click()
 sleep(5)
+
+driver.get("https://www.google.com")
+sleep(5)
+
+# SEARCH GOOGLE
+search_input =  driver.find_element_by_name("q")
+search_input.send_keys('site:linkedin.com/in/ AND "python" AND "Phoenix"')
+sleep(1)
+
+search_input.send_keys(Keys.RETURN)
+sleep(3)
+
+# GET PROFILES
+profiles = driver.find_elements(By.XPATH,"//*[@class='yuRUbf']/a[1]")
+profiles = [profile.get_attribute('href') for profile in profiles]
+sleep(3)
