d0c02f08665223e137cf3cf8a6ad89b1030e4585
HaroldDRF <95robots@gmail.com>
2022-01-23 10:19:10 -0700

    scrape urls and names from profiles

--- scrapy_bot.py
+++ scrapy_bot.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from time import sleep
+from parsel import Selector
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import csv
+
+import parameters
+
+writer = csv.writer(open(parameters.RESULT_FILE, 'w'))
+writer.writerow(['name', 'url'])
 
 driver = webdriver.Chrome("E:\Portfolio_Proj\Python\mapping\chromedriver")
 sleep(0.5)
@@ -16,22 +23,22 @@ sleep(3)
 import os
 
 username_input =  driver.find_element_by_name("session_key")
-username_input.send_keys( os.getenv('EMAIL_LINKEDIN'))
+username_input.send_keys( parameters.EMAIL_LINKEDIN)
 sleep(0.5)
 
 password_input =  driver.find_element_by_name("session_password")
-password_input.send_keys(os.getenv('PASS_LINKEDIN'))
+password_input.send_keys(parameters.PASS_LINKEDIN)
 sleep(0.5)
 
 driver.find_element_by_xpath('//button[text()="Sign in"]').click()
-sleep(5)
+sleep(15)
 
+# SEARCH GOOGLE
 driver.get("https://www.google.com")
 sleep(5)
 
-# SEARCH GOOGLE
 search_input =  driver.find_element_by_name("q")
-search_input.send_keys('site:linkedin.com/in/ AND "python" AND "Phoenix"')
+search_input.send_keys(parameters.SEARCH_QUERY)
 sleep(1)
 
 search_input.send_keys(Keys.RETURN)
@@ -40,4 +47,20 @@ sleep(3)
 # GET PROFILES
 profiles = driver.find_elements(By.XPATH,"//*[@class='yuRUbf']/a[1]")
 profiles = [profile.get_attribute('href') for profile in profiles]
-sleep(3)
+for profile in profiles:
+    driver.get(profile)
+    sleep(5)
+
+    sel = Selector(text=driver.page_source)
+
+    name = sel.xpath("//title/text()").extract_first().split("|")[0]
+    p_url = driver.current_url
+
+    print('\n')
+    print(name)
+    print(p_url)
+
+    writer.writerow([name, p_url])
+
+
+driver.quit()
